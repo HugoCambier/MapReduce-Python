2f5530d2359be23420bda7cbfadd47cf2621d52a
Hugo Cambier <hugocambier@MacBook-Pro-de-Hugo.local>
2017-10-04 17:00:13 +0200

    pseudo code

--- Functions.py
+++ Functions.py
@@ -1,34 +1,27 @@
-def mapper(tab):
-    print(tab)
-    #liste Mapper()
-    liste = []
-
-    a = 0
-    b = 0
-
-    for i in tab:
-        #Parcours ligne par ligne
-        for j in i:
-            #Parcours colonne par colonne
-
-            #Inscrit valeur et coordonnées dans la liste Mapper()
-            j = '{0},{1},{2}'.format(j, a, b)
-            liste.append(j)
-            b = b + 1
-
-        a = a + 1
-        b = 0
-
-    #On renvoie la liste Mapper()
-    return (liste)
+def map(rowIndex,values) :
+    #Split the values of the row (because we received a csv)
+    #Run the line, column by column
+    for(columnIndex = 0 to length(values)) :
+        #Column index from 0 to end of row
+        K = columnIndex
+        V = {rowIndex: values[columnIndex]}
+    #Return key + list of values(rowIndex, Value):
+    return {K:V}
 
 
-def reducer(obj):
-    #Split l'objet recu, composé d'une valeur et de ses coordonnées
-    split = obj.split(',')
-    a = split[0]
-    x = split[1]
-    y = split[2]
+def shuffleAndSort():
+    #Gather elements with the same key and sort them
 
-    #Retourne valeur, nouveau x, et nouveau y
-    return (a, y, x)
+
+#Normally, I understood that inversion of rows/columns had to be done
+#in the suffleAndSort. I did it in Reducer() to receive good coordinates
+#and make the right inversed table.
+def reduce(K,V[]) :
+    #At the entrance: K,V[] -> {y:{x, val}}
+    for(x in V[]) :
+        transposedColumnIndex = rowKey(k)
+        transposedRowIndex = columnKey
+    #At the end: K,V[] -> {x:{y, val}}
+    return {transposedRowIndex: {transposedColumnIndex, val}}
+
+
--- Main.py
+++ Main.py
@@ -1,28 +1,40 @@
 import numpy as np
 from Functions import *
 
-#Déclaration tableau initial
-tab = np.array([[3, 4, 5, 6],[1, 2, 3, 4],[9, 8, 7, 10],[56, 21, 45, 32]])
-#Déclaration tableau final
-tabReduce = np.array([[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]])
+#Initialize initial tab
+#We load and read a csv file
+tab
 
-#Appel à la fonction Mapper()
-tabMap = mapper(tab)
+#Example with input lines:
+#a b c d
+#1 2 3 4
 
-#Inscrit la liste retournée par Mapper()
-print(tabMap)
+#Call Mapper() function row by row
+for row in tab:
+    mapper(rowIndex, row)
 
-for i in tabMap:
-    #Parcours de la liste retournée par le Mapper()
-    #Appel à la fonction Reducer()
-    #Retourne la valeur, ainsi que les coordonnées inversées
-    (val,x,y) = reducer(i)
-    #Cast les coordonnées en int pour pouvoir parcourir le tableau final
-    x = int(x)
-    y = int(y)
-    #Inscrit la bonne valeur à la bonne place dans le tableau
-    tabReduce[x][y] = val
+#Example:
+#end of the mapper():
+#map(0,[a b c d]) #=> {0:{0,a}},  {1:{0,b}},  {2:{0,c}},  {3:{0,d}}
+#map(1,[1 2 3 4]) #=> {0:{1,1}},  {1:{1,2}},  {2:{1,3}},  {3:{1,4}}
 
-#Résultat: tableau inversé
-print(tabReduce)
+
+#(((
+#Shuffle and sort, sort and order by key (here, column index)
+#)))
+
+    
+#Call Reducer() function key by key
+for row in tabMapped:
+    reducer(key, row[])
+    
+#Example:
+#end of the reducer()
+#{0:   [{0,a}, {1, 1}]} #-> [a 1]
+#{1:   [{0,b}, {1, 2}]} #-> [b 2]
+#{2:   [{0,c}, {1, 3}]} #-> [c 3]
+#{3:   [{0,d}, {1, 4}]} #-> [d 4]
+
+#Example result: inversed tab
+
 
